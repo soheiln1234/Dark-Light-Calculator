731c8e712484cfc676a6c0c6244529718960d7cb
Soheil Nikroo <soheiln1234@gmail.com>
2021-09-17 08:51:31 -0700

    Basic animation added.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,24 @@
+// Get the mode from local storage
+window.onload = () => {
+  const body = document.body;
+
+  let mode = localStorage.getItem("mode");
+  if (mode === "dark") {
+    body.classList.add("dark-mode");
+  } else {
+    body.classList.remove("dark-mode");
+  }
+};
+
 // Changing mode of calculatore
 const themeChange = (mode) => {
   const body = document.body;
 
   if (mode === "light") {
     body.classList.remove("dark-mode");
+    localStorage.setItem("mode", "light");
   } else {
+    localStorage.setItem("mode", "dark");
     body.classList.add("dark-mode");
   }
 };
